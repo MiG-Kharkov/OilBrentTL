3bd9d359f70c654a51aef127c90083e91f0b98e4
Igor Manzhos <manzhos.igor@gmail.com>
2017-06-02 10:26:38 +0300

    Data Slicing

--- DataSliceing.R
+++ DataSliceing.R
@@ -1,2 +1,24 @@
 # Part 3
 # Подготовка данных для моделирования
+
+sizeWindow <- 30
+allRecords <- length(oil.xts)
+trainLength <- round(allRecords * 0.9)
+
+windowBasedData <- NULL
+for (i in 1:(allRecords - sizeWindow)) {
+  sliceData <- as.data.frame(t(ds$V2[i:(i + sizeWindow)]))
+  windowBasedData <- rbind(windowBasedData, sliceData)
+}
+
+windowBasedData %>%
+  select(num_range("V", sizeWindow:(sizeWindow + 1))) %>%
+  mutate(Raise = ifelse(.[1] < .[2] , 1, 0)) -> raiseFactor
+
+windowBasedData <- cbind(windowBasedData[1:sizeWindow], raiseFactor[3])
+
+windowBasedData %>%
+  slice(1:trainLength) -> trainData
+windowBasedData %>%
+  slice((trainLength + 1):(allRecords - sizeWindow)) -> testData
+        
--- GetData.R
+++ GetData.R
@@ -1,2 +1,19 @@
+# Part 1
+# Блок загрузки данных
+if (!require("tseries")) install.packages("tseries")
+if (!require("forecast")) install.packages("forecast")
+library(ggplot2)
+library(data.table)
+library(dplyr)
+library(tseries)
+library(forecast)
+library(zoo)
+library(xts)
+library(dplyr)
+library(grDevices)
 # Data set 
 # https://www.eia.gov/dnav/pet/hist/LeafHandler.ashx?n=PET&s=rbrte&f=D
+ds <- fread("Europe_Brent_Spot_Price_FOB.csv", skip = 5)
+ds$V1 <- as.Date(ds$V1, format = "%m/%d/%Y")
+
+oil.xts <- xts(ds$V2, order.by = ds$V1)
